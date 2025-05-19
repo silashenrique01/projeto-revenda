d162e9c07f2e6a084addfa646439b0ab94c48594
silas <silas.henrique99@outlook.com>
2025-05-18 21:28:47 -0300

    teste revenda

--- Back/src/Tests/Domain/RevendaTest.cs
+++ Back/src/Tests/Domain/RevendaTest.cs
@@ -5,12 +5,19 @@ using NUnit.Framework;
 
 namespace Tests.Domain
 {
-    public class Tests
+    public class RevendaTest
     {
         [Test]
         public void Setup()
         {
 
+           
+
+        }
+
+        [Test]
+        public void DeveCriarRevenda_QuandoCnpjValido()
+        {
             var contato = new Contato("Nome", "Sobrenome", true);
             var telefone = new Telefone("999999999");
             var telefones = new List<Telefone>();
@@ -22,11 +29,6 @@ namespace Tests.Domain
             contatos.Add(contato);
             var revenda = new Revenda("12345678000195", "Razão Social", "Nome Fantasia", "contato@email.com", telefones, contatos, enderecos, null);
 
-        }
-
-        public void DeveCriarRevenda_QuandoCnpjValido(Revenda revenda)
-        {
-
             var validarCnpj = new CNPJ(revenda.Cnpj);
             var teste = validarCnpj.Validar();
 
@@ -34,11 +36,21 @@ namespace Tests.Domain
             Assert.IsTrue(teste);
         }
 
-        public void DeveCriarRevenda_QuandoCnpjObrigatorio(Revenda revenda)
+        [Test]
+        public void DeveCriarRevenda_QuandoCnpjObrigatorio()
         {
+            var contato = new Contato("Nome", "Sobrenome", true);
+            var telefone = new Telefone("999999999");
+            var telefones = new List<Telefone>();
+            telefones.Add(telefone);
+            var endereco = new Endereco("rua", 123, "04166003", "bairro", "são paulo", "sp", "complemento", "ponto de referencia");
+            var enderecos = new List<Endereco>();
+            enderecos.Add(endereco);
+            var contatos = new List<Contato>();
+            contatos.Add(contato);
+            var revenda = new Revenda("12345678000195", "Razão Social", "Nome Fantasia", "contato@email.com", telefones, contatos, enderecos, null);
 
-            Assert.IsNotNull(validarCnpj);
-            Assert.IsTrue(teste);
+            Assert.IsNotNull(revenda.Cnpj);
         }
     }
 }
